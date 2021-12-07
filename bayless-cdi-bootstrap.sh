6c892ebe0c62ec151b082bde25daa7bfbb0e7378
nstbayless <nstbayless@gmail.com>
2021-12-07 15:43:45 -0800

    update

--- bayless-cdi-bootstrap.sh
+++ bayless-cdi-bootstrap.sh
@@ -1,9 +1,13 @@
 # usage:
 #
-#   bash <(curl -s https://raw.githubusercontent.com/nstbayless/lakka-script/main/bayless-cdi-bootstrap.sh)
+#   export PASSWORD='<Bayless CD-i master password>' # (optional)
+#   curl https://raw.githubusercontent.com/nstbayless/lakka-script/main/bayless-cdi-bootstrap.sh | /bin/bash
 #
 # this script bootstraps a Bayless CD-i
 # it must be run on a Lakka device, from any directory.
+#
+# Note: master password is NOT the Lakka root password (i.e. 'root'). If you
+# have been given this script, then you should know what the password is.
 
 if [ ! -d /storage ] || [ ! -f /usr/bin/retroarch ]
 then
@@ -26,7 +30,7 @@ assert_has_command bash
 assert_has_command openssl
 
 set -e
-SETUP_DIRECTORY=`mktemp -d`
+SETUP_DIRECTORY=`mktemp -d --suffix=bayless-cdi-bootstrap`
 cd $SETUP_DIRECTORY
 echo "using '$SETUP_DIRECTORY' as temporary workspace"
 
@@ -37,24 +41,30 @@ DECRYPT="openssl aes-256-cbc -d -a -iter 6 -pass"
 while [ -z "$PASSWORD" ]
 do
     echo "Enter Bayless CD-i master password:"
-    read PASSWORD
+    # we need to read from /dev/tty so that this works even during curl-bash
+    read PASSWORD < /dev/tty 
 done
 
 # check that password was correct
 if ! (echo "U2FsdGVkX193ldYerIk4JvcGPdv3Fz2NLR51g5wY11E=" | $DECRYPT pass:$PASSWORD)
 then
     echo "Incorrect password. Please contact administrator for assistance."
+    echo "To resolve this error, please use the following command before running this script:"
+    echo ""
+    echo "  export PASSWORD='<Bayless CD-i master password>'"
+    echo ""
     exit 1
 fi
 
 REPO_FILE_ACCESS_URL=https://raw.githubusercontent.com/nstbayless/lakka-script/main
 
-#download pre-requisite files
+#download pre-requisite binaries
 mkdir bin
 cd bin
 wget $REPO_FILE_ACCESS_URL/bin/git
 cd ..
 
+# download pre-requisite libs
 mkdir lib
 cd lib
 wget $REPO_FILE_ACCESS_URL/lib/libc.so.6
@@ -63,5 +73,6 @@ wget $REPO_FILE_ACCESS_URL/lib/libpthread.so.0
 wget $REPO_FILE_ACCESS_URL/lib/libz.so.1
 cd ..
 
+# add binaries and libraries to the PATH
 export PATH="$PATH:$SETUP_DIRECTORY/bin"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$SETUP_DIRECTORY/lib"
